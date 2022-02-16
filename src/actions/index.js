f33e5e309271a9dfd45a510769d3ad3e8d832fd3
Tatyana Asenova <tatyana.asenova@mentormate.com>
2022-02-16 18:31:22 +0200

    Created constants and actions for the images statistics to be fetched

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { IMAGES } from '../constants';
+import { IMAGES, STATS } from '../constants';
 
 
 const loadImages = () => ({
@@ -15,8 +15,29 @@ const setError = error => ({
     error
 })
 
+
+
+const loadImageStats = id => ({
+    type: STATS.LOAD,
+    id,
+});
+
+const setImageStats = (id, downloads) => ({
+    type: STATS.LOAD_SUCCESS,
+    id,
+    downloads,
+});
+
+const setImageStatsError = id => ({
+    type: STATS.LOAD_FAIL,
+    id,
+});
+
 export {
     loadImages,
     setImages,
-    setError
+    setError,
+    loadImageStats,
+    setImageStats,
+    setImageStatsError,
 }
