5f4a9ec8b58efa2f5dee816662746224edf53103
Tatyana Asenova <tatyana.asenova@mentormate.com>
2022-02-16 18:19:03 +0200

    Fully working load more functionality

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ const URL = 'https://api.unsplash.com/photos/'
 
 //should return a promise so saga can be suspended untill it returns
 const fetchImages = async page => {
-    const response = await fetch(`${URL}${KEY}&per_page=10&page=${page}sss`)
+    const response = await fetch(`${URL}${KEY}&per_page=10&page=${page}`)
     const data = await response.json();
     if (response.status >= 400) {
         throw new Error(data.errors)
